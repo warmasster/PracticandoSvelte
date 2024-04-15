07480045b9a91b0a0e41bcbae1ff8782d3b10f04
Fernando Hernandez <ferndohm99emf@gmail.com>
2024-04-15 15:58:07 +0200

    añade funcion ed generacion de numeros aleatorios

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
     import "../app.css";
     let num = 0;
-
+    let nums = [];
+    let rand = 0;
     function increment(){
         num++;
     }
@@ -9,28 +10,49 @@
     function decrement(){
         num = ((num > 0) ? (num = num-1) : (num = 0))
     }
+
+    function gen_nums(){
+        console.log(nums);
+        nums = [];
+        for(let i = 0; i < num; i++){
+            rand = Math.random()*100;
+            nums.push(Math.floor(rand));
+        }
+    }
 </script>
 
 <div class="flex justify-center darker-bg nav">
     <h1 class="title light">Numero aleatorio</h1>
 </div>
 
-<div class="flex justify-center pt-6">
-    <button class="mx-10 darker-bg btn hover:cursor-pointer" on:click={decrement}>
-        <span class="minus-simbol"></span>
-    </button>
-    
-    <div class="w-24 text-center ">
-        <h2 class="text-5xl font-bold sm:text-6xl light">{num}</h2>
+<div class="flex flex-col flex-wrap justify-center pt-6 px-100 standalone-center">
+    <div class="flex">
+        <button class="mx-10 darker-bg btn hover:cursor-pointer" on:click={decrement}>
+            <span class="minus-simbol"></span>
+        </button>
+        
+        <div class="w-24 text-center ">
+            <h2 class="text-5xl font-bold sm:text-6xl light">{num}</h2>
+        </div>
+        
+        <button class="mx-10 darker-bg btn hover:cursor-pointer" on:click={increment}>
+            <span class="plus-simbol-1"></span>
+            <span class="plus-simbol-2"></span>
+        </button>
     </div>
-    
-    <button class="mx-10 darker-bg btn hover:cursor-pointer" on:click={increment}>
-        <span class="plus-simbol-1"></span>
-        <span class="plus-simbol-2"></span>
-    </button>
 
+    <div class="pt-7">
+        <button class="flex flex-wrap justify-center m-0 mx-10 darker-bg btn-l hover:cursor-pointer" on:click={gen_nums}>
+            <h2 class="text-2xl font-bold sm:text-4xl light">Generar numeros</h2>
+        </button>
+    </div>
 </div>
 
+{#if nums.length > 0}
+    {#each nums as rand_num, i}
+        <div><h1>{rand_num}</h1></div>
+    {/each}
+{/if}
 
 <style lang="postcss">
     
@@ -44,28 +66,33 @@
     .light{color: rgb(119, 176, 170)}
     .lighter{color: rgb(227, 254, 247)}
     
+    .standalone-center {
+        display: table;
+        margin-right: auto;
+        margin-left: auto;
+    }
     .minus-simbol{
          background-color: rgb(119, 176, 170);
-         border-radius: 0.5vh;
-         height: 9vh;
-         width: 1.5vh;
+         border-radius: 2px;
+         height: 3rem;
+         width: 0.5rem;
          rotate: 90deg;
     }
     
     .plus-simbol-2{
         position: absolute;
          background-color: rgb(119, 176, 170);
-         border-radius: 0.5vh;
-         height: 9vh;
-         width: 1.5vh;
+         border-radius: 2px;
+         height: 3rem;
+         width: 0.5rem;
          rotate: 90deg;
     }
     .plus-simbol-1{
         position: absolute;
          background-color: rgb(119, 176, 170);
-         border-radius: 0.5vh;
-         height: 9vh;
-         width: 1.5vh;
+         border-radius: 2px;
+         height: 3rem;
+         width: 0.5rem;
     }
     
     .btn {
@@ -73,8 +100,18 @@
         justify-content: center;
         align-items: center;
         border-radius: 10px;
-        height: 12vh;
-        width: 12vh;
+        height: 5rem;
+        width: 5rem;
+        padding: 5px;
+    }
+
+    .btn-l {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 10px;
+        height: 4rem;
+        width: 21rem;
         padding: 5px;
     }
     .title {
